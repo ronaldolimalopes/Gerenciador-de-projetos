82e644d6b0716e61c6e44f7b8dbba77caad99847
Ronaldo Lopes <ronaldo.lopes@prodeb.ba.gov.br>
2022-08-23 14:42:08 -0300

    Criando getResponder e getHttpErrorHandler

--- src/App.php
+++ src/App.php
@@ -2,6 +2,7 @@
 
 namespace Ronaldolopes\GerenciadorProjetos; 
 
+use Pimple\Container;
 use Ronaldolopes\GerenciadorProjetos\Exceptions\HttpException;
 use Ronaldolopes\GerenciadorProjetos\Router;
 use Ronaldolopes\GerenciadorProjetos\Response;
@@ -14,9 +15,13 @@ class App
         'before' => [],
         'after' => [] 
     ];
-    public function __construct($container)
+    public function __construct(Container $container = null)
     {
         $this->container = $container;
+
+        if($this->container === null){
+            $this->container = new Container;
+        }
     }
 
     public function addMiddleware($on, $callback)
@@ -34,13 +39,39 @@ class App
         return $this->container['router'];
     }
 
+    public function getResponder()
+    {
+        if(!$this->container->offsetExists('responder')){
+            $this->container['responder'] = function(){
+                return new Response;
+            };
+        };
+        return $this->container['responder'];
+    }
+
+    public function getHttpErrorHandler()
+    {
+        if($this->container->offsetExists('httpErrorHandler')){
+            
+            $this->container['httpErrorHandler'] = function($c){
+                header('Content-Type: application/json');
+                $response = json_encode([
+                    'code' => $c['exception']->getCode(), 
+                    'error' => $c['exception']->getMessage()
+                ]);
+                return $response;
+            };
+        };
+        return $this->container['httpErrorHandler'];
+    }
+
     public function run()
     {
         try {
     
             $result = $this->getRouter()->run();
             
-            $response = new Response;
+            $response = $this->getResponder();
             
             $params = [
                 'container' => $this->container,
@@ -58,7 +89,8 @@ class App
             }
             
         } catch (HttpException $e) {
-            echo json_encode(['error' => $e->getMessage()]);
+           $container['exception'] = $e;
+           echo $this->getHttpErrorHandler();
         }
     }
 }
