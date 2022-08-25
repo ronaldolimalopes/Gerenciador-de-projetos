18346cdc191b496f49ed94d51743fe4f04417cc6
Ronaldo Lopes <ronaldolopes@MacBook-Pro-de-Ronaldo.local>
2022-08-25 15:34:09 -0300

    Implementação da abstração Model

--- app/src/Models/Users.php
+++ app/src/Models/Users.php
@@ -1,82 +1,10 @@
 <?php
 
 namespace App\Models;
-use Pimple\Container;
-use Ronaldolopes\GerenciadorProjetos\QueryBuilder;
 
-class Users
+use Ronaldolopes\GerenciadorProjetos\Model;
+
+class Users extends Model
 {
-    private $db;
-    private $event;
-
-    public function __construct(Container $container)
-    {
-        $this->db = $container['db'];
-        $this->event = $container['events'];
-    }
-
-    public function get($conditions)
-    {
-        $queryBuilder = new QueryBuilder;
-        $query = $queryBuilder->select('users')->where($conditions)->getData();
-        
-        $stmt = $this->db->prepare($query->sql);
-        $stmt->execute($query->bind);
-        return $stmt->fetch(\PDO::FETCH_ASSOC);
-    }
-
-    public function create(array $data)
-    {
-        $this->event->trigger('creating.users', null, $data);
-        
-        $queryBuilder = new QueryBuilder;
-        $query = $queryBuilder->insert('users', $data)->getData();
-        
-        $stmt = $this->db->prepare($query->sql);
-        $stmt->execute(array_values($query->bind));
-        
-        $result = $this->get($this->db->lastInsertId());
-
-        $this->event->trigger('created.users', null, $result);
-
-        return $result;
-    }
-
-    public function all()
-    {
-        $queryBuilder = new QueryBuilder;
-        $query = $queryBuilder->select('users')->getData();
-        
-        $stmt = $this->db->prepare($query->sql);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function update($conditions, array $data)
-    {
-        $this->event->trigger('updating.users', null, $data);
-
-        $queryBuilder = new QueryBuilder;
-        $query = $queryBuilder->update('users', $data)->where($conditions)->getData();
-        
-        $stmt = $this->db->prepare($query->sql);
-        $stmt->execute(array_values($query->bind));
-
-        $result = $this->get($conditions);
-        $this->event->trigger('updated.users', null, $data);
-
-        return  $result;
-    }
-
-    public function delete($conditions)
-    {
-        $result = $this->get($conditions);
-        $queryBuilder = new QueryBuilder;
-        $query = $queryBuilder->delete('users')->where($conditions)->getData();
-        
-        $stmt = $this->db->prepare($query->sql);
-        $stmt->execute($query->bind);
-       
-        return  $result;
-    }
+    protected $table = 'users';
 }
---
+++ src/Model.php
@@ -0,0 +1,89 @@
+<?php
+namespace Ronaldolopes\GerenciadorProjetos;
+
+use Pimple\Container;
+use Ronaldolopes\GerenciadorProjetos\QueryBuilder;
+
+class Model
+{
+    private $db;
+    private $event;
+    protected $table = 'users';
+
+    public function __construct(Container $container)
+    {
+        $this->db = $container['db'];
+        $this->event = $container['events'];
+
+        if(!$this->table){
+            $table = explode('\\', get_called_class());
+            $table = array_pop($table);
+            $this->table = strtolower($table);
+        }
+    }
+
+    public function get($conditions)
+    {
+        $queryBuilder = new QueryBuilder;
+        $query = $queryBuilder->select($this->table)->where($conditions)->getData();
+        
+        $stmt = $this->db->prepare($query->sql);
+        $stmt->execute($query->bind);
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function create(array $data)
+    {
+        $this->event->trigger('creating.'.$this->table, null, $data);
+        
+        $queryBuilder = new QueryBuilder;
+        $query = $queryBuilder->insert($this->table, $data)->getData();
+        
+        $stmt = $this->db->prepare($query->sql);
+        $stmt->execute(array_values($query->bind));
+        
+        $result = $this->get($this->db->lastInsertId());
+
+        $this->event->trigger('created.'.$this->table, null, $result);
+
+        return $result;
+    }
+
+    public function all()
+    {
+        $queryBuilder = new QueryBuilder;
+        $query = $queryBuilder->select($this->table)->getData();
+        
+        $stmt = $this->db->prepare($query->sql);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function update($conditions, array $data)
+    {
+        $this->event->trigger('updating.'.$this->table, null, $data);
+
+        $queryBuilder = new QueryBuilder;
+        $query = $queryBuilder->update($this->table, $data)->where($conditions)->getData();
+        
+        $stmt = $this->db->prepare($query->sql);
+        $stmt->execute(array_values($query->bind));
+
+        $result = $this->get($conditions);
+        $this->event->trigger('updated.'.$this->table, null, $data);
+
+        return  $result;
+    }
+
+    public function delete($conditions)
+    {
+        $result = $this->get($conditions);
+        $queryBuilder = new QueryBuilder;
+        $query = $queryBuilder->delete($this->table)->where($conditions)->getData();
+        
+        $stmt = $this->db->prepare($query->sql);
+        $stmt->execute($query->bind);
+       
+        return  $result;
+    }
+}
